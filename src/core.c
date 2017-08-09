3fff753a144475076dc2b30a24b8abc3c7fccc9b
Brandon Azad <bazad@cs.stanford.edu>
2017-08-08 21:49:14 -0700

    Search all host special ports

--- src/core.c
+++ src/core.c
@@ -8,9 +8,12 @@
  * found_kernel_port
  *
  * Description:
- * 	Test if the kernel port has been successfully retrieved.
+ * 	Test if the kernel task port has been successfully retrieved.
  */
 static bool found_kernel_port() {
+	if (kernel_task == MACH_PORT_NULL) {
+		return false;
+	}
 	pid_t pid;
 	kern_return_t kr = pid_for_task(kernel_task, &pid);
 	if (kr != KERN_SUCCESS || pid != 0) {
@@ -22,23 +25,24 @@ static bool found_kernel_port() {
 }
 
 /*
- * try_host_special_port_4
+ * try_host_special_ports
  *
  * Description:
- * 	Try to obtain the kernel task port via host special port 4.
+ * 	Try to obtain the kernel task port via any of the host special ports.
  */
-static bool try_host_special_port_4() {
+static bool try_host_special_ports() {
 	mach_port_t host = mach_host_self();
 	if (host == MACH_PORT_NULL) {
 		return false;
 	}
-	kern_return_t kr = host_get_special_port(host, 0, 4, &kernel_task);
-	mach_port_deallocate(mach_task_self(), host);
-	if (kr != KERN_SUCCESS || !found_kernel_port()) {
-		return false;
+	for (int port_id = 0; port_id <= HOST_MAX_SPECIAL_PORT; port_id++) {
+		host_get_special_port(host, 0, port_id, &kernel_task);
+		if (found_kernel_port()) {
+			break;
+		}
 	}
-	assert(kernel_task != 0);
-	return true;
+	mach_port_deallocate(mach_task_self(), host);
+	return (kernel_task != MACH_PORT_NULL);
 }
 
 /*
@@ -48,18 +52,14 @@ static bool try_host_special_port_4() {
  * 	Try to obtain the kernel task port via task_for_pid.
  */
 static bool try_task_for_pid_0() {
-	kern_return_t kr = task_for_pid(mach_task_self(), 0, &kernel_task);
-	if (kr != KERN_SUCCESS || !found_kernel_port()) {
-		return false;
-	}
-	assert(kernel_task != 0);
-	return true;
+	task_for_pid(mach_task_self(), 0, &kernel_task);
+	return found_kernel_port();
 }
 
 bool core_load() {
-	if (try_host_special_port_4() || try_task_for_pid_0()) {
+	if (try_host_special_ports() || try_task_for_pid_0()) {
 		return true;
 	}
-	error_core("could not obtain kernel task port via host_get_special_port(4) or task_for_pid(0)");
+	error_core("could not obtain kernel task port via host_get_special_port() or task_for_pid(0)");
 	return false;
 }
